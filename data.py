10041058b07d867a4dda1802adec4bcc69446b11
Emmanuel <55893421+emmanuel-mfum@users.noreply.github.com>
2021-02-14 00:06:44 -0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,79 @@
+question_data = [
+    {
+        "category": "General Knowledge",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The Great Wall of China is visible from the moon.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "It is automatically considered entrapment in the United States if the police sell you illegal substances without revealing themselves.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean", "difficulty": "easy",
+        "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Bulls are attracted to the color red.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": " 'Ananas' is mostly used as the word for Pineapple in other languages.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The color orange is named after the fruit.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "One of Donald Trump's 2016 Presidential Campaign promises was to build a border wall between the United States and Mexico.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Scotland voted to become an independent country during the referendum from September 2014.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Vietnam's national flag is a red star in front of a yellow background.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A pasodoble is a type of Italian pasta sauce.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
